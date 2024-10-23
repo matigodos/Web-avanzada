6210d89c5a93c297dc3ca90240061e2055b7a6df
matigodos <matildemaria.godos@mail.ucv.es>
2024-10-23 12:29:54 +0200

    ultimo commmit clase

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,52 @@ const b=randInt(0,255);
 
 elem.style.fill='rgb('+r+','+g+','+b+')'
 }
+
+function isFocused(elem){
+elem.style.fill="#68c33f";
+}
+
+function isNotFocused(elem){
+    elem.style.fill="white";
+    }
+
+function throwDie(elem){
+        const rand=randInt(0,5);
+        elem.children[0].setAttribute("d",dice[rand]);
+        }
+        
+        function changeDadoColorIn(elem) {
+            const r=randInt(0,255);
+            const g=randInt(0,255);
+            const b=randInt(0,255);
+            
+            elem.style.fill='rgb('+r+','+g+','+b+')'
+            }
+            
+            function changeDadoColorOut(elem) {
+                const r=randInt(0,255);
+                const g=randInt(0,255);
+                const b=randInt(0,255);
+                
+                elem.style.fill='rgb('+r+','+g+','+b+')'
+                }
+
+ 
+
+function Cambiodefondo(elem){
+const miVar=document.getElementsByTagName("main");
+miVar[0].style.backgroundImage = "url('paisajenocturno.jpg')";
+
+const cabecera=document.getElementsByTagName("header");
+cabecera[0].style.backgroundColor="black";
+
+const titulo=document.getElementsByTagName("h1");
+titulo[0].style.color="white";
+
+const footer=document.getElementsByTagName("footer");
+footer[0].style.backgroundColor="black";
+
+const titulofooter=document.getElementsByTagName("h2");
+titulofooter[0].style.color="white";
+
+}
